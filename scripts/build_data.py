3a3a84ccc56a525d5eb02f60cb4570f52116e27b
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-08-24 16:33:15 +0100

    Set source as active if source org is populated

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,9 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_meta(meta):
+  for i in range(1,7):
+    if 'source_organisation_'+str(i) in meta and meta['source_organisation_'+str(i)] != '':
+      meta['source_active'+str(i)]=True
+  return meta
+
+open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
