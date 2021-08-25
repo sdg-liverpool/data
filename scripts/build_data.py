64909370cc13d2fe10bac36f1fca4be1c7cac901
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-08-25 12:17:40 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -3,7 +3,7 @@ from sdg.open_sdg import open_sdg_build
 def alter_meta(meta):
   for i in range(1,7):
     if 'source_organisation_'+str(i) in meta and meta['source_organisation_'+str(i)] != '':
-      meta['source_active'+str(i)]=True
+      meta['source_active_'+str(i)]=True
   return meta
 
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
